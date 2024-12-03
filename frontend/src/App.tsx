9d05d57c2a87d49d84f143124058e9ae822f3e71
gabrielherreca <2gaherre12@gmail.com>
2024-12-02 20:00:10 -0500

    Refactoring code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
 import LoginScreen from "./login/LoginScreen.tsx";
-import Layout from  "./layout/Layout.tsx";
+
+
+
 function App() {
 
   return (
     <>
-        <Layout name={'Josefina Agurre'}></Layout>
+        <LoginScreen/>
+
     </>
   )
 }
---
+++ frontend/src/layout/components/Example_data.tsx
@@ -0,0 +1,15 @@
+export const header =  ['Nombre', 'Apellidos', 'Especialidad', 'Contrato', "Asignaturas", "Experiencia", "Valoracion"];
+
+export const rows = [
+    ['Juan', 'Souto Morales', 'Matematica Discreta', 'Temporal', 'Algebra', '5 años', '4.5'],
+    ['Maria', 'Gomez', 'Fisica', 'Indefinido', 'Mecanica', '10 años', '4.8'],
+    ['Pedro', 'Jimenez', 'Quimica', 'Temporal', 'Organica', '2 años', '4.2'],
+    ['Ana', 'Lopez', 'Biologia', 'Indefinido', 'Celular', '15 años', '4.9'],
+    ['Luis', 'Garcia', 'Historia', 'Temporal', 'Antigua', '3 años', '4.1'],
+    ['Sara', 'Martinez', 'Lengua', 'Indefinido', 'Español', '8 años', '4.7'],
+    ['Carlos', 'Fernandez', 'Ingles', 'Temporal', 'Britanico', '1 años', '4.0'],
+    ['Paula', 'Sanchez', 'Educacion Fisica', 'Indefinido', 'Atletismo', '12 años', '4.6'],
+    ['David', 'Torres', 'Informatica', 'Temporal', 'Python', '4 años', '4.3'],
+    ['Laura', 'Vega', 'Musica', 'Indefinido', 'Piano', '7 años', '4.8'],
+    ['Raul', 'Rojas', 'Arte', 'Temporal', 'Pintura', '6 años', '4.4'],
+];
