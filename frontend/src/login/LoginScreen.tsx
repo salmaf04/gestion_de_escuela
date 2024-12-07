8ec7350998bcc0f580e753a31e822ba05dcddc80
Adrian <adrianalejandrosm03@gmail.com>
2024-12-07 13:13:33 -0500

    FIX   login ui design

--- frontend/src/login/LoginScreen.tsx
+++ frontend/src/login/LoginScreen.tsx
@@ -1,20 +1,36 @@
-
+import {Canvas} from "@react-three/fiber";
+import {
+    OrbitControls,
+     SpotLight,
+} from "@react-three/drei";
 import Formulario from "./components/Formulario.tsx";
+import Universitario from "./components/Universitario";
 
 function LoginScreen() {
 
     return (
-        <div className={'h-svh w-svw flex bg-gradient-to-br from-indigo-200 to-indigo-400 justify-center items-center'}>
-            <div className={'h-3/4 w-3/4 bg-white shadow-lg shadow-indigo-400 flex rounded-lg'}>
-                <div className={' w-1/2 bg-blue-5 p-2'}>
-                    {/*<Canvas>
-                        <SpotLight position={[0, 10, 0]} angle={10} intensity={200} distance={250}/>
-                        <SpotLight position={[4, -2, 4]} intensity={200} distance={250}/>
-                        <PerspectiveCamera near={0.1}/>
-                        <OrbitControls/>
-                        <Universitario/>
-                    </Canvas>*/}
-                    <img src={'/sign-in.jpg'} className={'h-full hue-rotate-15'} alt={'Imagen de login'}/>
+        <div className={'h-svh w-svw flex bg-gradient-to-br from-indigo-400 to-indigo-500 justify-center items-center'}>
+            <div className={'h-3/4 w-3/4 bg-white shadow-lg shadow-indigo-600 flex rounded-lg'}>
+                <div className={' w-1/2 bg-indigo-500 p-10'}>
+                    <div className={'w-fit'}>
+                        <h1 className={'text-white font-bold text-4xl'}>EduManager</h1>
+                        <p className={'text-lg text-indigo-50 text-end'}>te da la bienvenida</p>
+                    </div>
+                    <div className={'w-full h-full '}>
+                        <Canvas>
+                            <SpotLight position={[0,10,0]} angle={10} intensity={200} distance={250}/>
+                            <SpotLight position={[0,-10,0]} angle={10} intensity={200} distance={250}/>
+                            <Universitario/>
+                            <OrbitControls enableZoom={false}/>
+                            {/*<mesh>
+                    <boxGeometry/>
+                    <meshBasicMaterial color="hotpink"/>
+                </mesh>*/}
+                        </Canvas>
+                    </div>
+
+
+
                 </div>
                 <div className={'h-full w-1/2'}>
                     <Formulario />
--- frontend/src/login/components/Formulario.tsx
+++ frontend/src/login/components/Formulario.tsx
@@ -23,10 +23,13 @@ export default function Formulario() {
                 console.log(res.text())
             })
         }
-    } className={"size-full flex flex-col items-center justify-around p-10 text-indigo-400"}>
-        <h1 className={"text-indigo-400 surface font-bold text-4xl"}>Bienvenido</h1>
-        <div className={"w-2/3 space-y-8 translate-y-4"}>
-            <div>
+    } className={"size-full flex flex-col items-center justify-around p-10 text-indigo-500"}>
+        <h1 className={" surface font-bold text-3xl"}>Iniciar Sesión</h1>
+        <div className={"w-2/3 space-y-4 translate-y-2"}>
+            <div className={'group'}>
+                <h2 className={'text-indigo-500 text-xs font-semibold opacity-0 group-focus-within:opacity-100 translate-y-5 group-focus-within:translate-y-0 transition-all'}>
+                    Usuario
+                </h2>
                 <input
                     value={user.username}
                     placeholder={"Usuario"} type="text"
@@ -40,31 +43,38 @@ export default function Formulario() {
                         }
                     }/>
             </div>
-            <div className={"relative flex items-center"}>
-                <input placeholder={"Contraseña"} type={isPassVisible ? "text" : "password"}
-                       value={user.password}
-                       required={true}
-                       onChange={
-                            (newText) => {
-                                 setUser(prevState => {
-                                      return {...prevState, password: newText.target.value}
-                                 })
-                            }
-                       }
-                       className={" rounded-lg h-10 w-full p-3 text-indigo-950 focus:outline-indigo-600 bg-indigo-50"}/>
-                <img className={"absolute end-2 cursor-pointer scale-90"}
-                     alt={"Cambiar visibilidad"}
-                     onClick={
-                            (e) => {
-                                e.preventDefault();
-                                setIsPassVisible(!isPassVisible);
-                     }}
-                     src={isPassVisible ? VisibilityOff : VisibilityOn}/>
+            <div className={'group'}>
+                <h2 className={'text-indigo-500 text-xs font-semibold opacity-0 group-focus-within:opacity-100 translate-y-5 group-focus-within:translate-y-0 transition-all'}>
+                    Contraseña
+                </h2>
+                <div className={"relative flex items-center"}>
+
+                    <input placeholder={"Contraseña"} type={isPassVisible ? "text" : "password"}
+                           value={user.password}
+                           required={true}
+                           onChange={
+                               (newText) => {
+                                   setUser(prevState => {
+                                       return {...prevState, password: newText.target.value}
+                                   })
+                               }
+                           }
+                           className={" rounded-lg h-10 w-full p-3 text-indigo-950 focus:outline-indigo-600 bg-indigo-50"}/>
+                    <img className={"absolute end-2 cursor-pointer scale-90"}
+                         alt={"Cambiar visibilidad"}
+                         onClick={
+                             (e) => {
+                                 e.preventDefault();
+                                 setIsPassVisible(!isPassVisible);
+                             }}
+                         src={isPassVisible ? VisibilityOff : VisibilityOn}/>
+                </div>
             </div>
+
         </div>
 
         <button
-            className={`${isLoading? 'bg-indigo-200': 'bg-indigo-400 shadow-md'}  shadow-indigo-300 transition-all text-indigo-50 font-semibold hover:scale-105 rounded-lg py-3 w-2/3 hover:bg-indigo-500`}
+            className={`${isLoading ? 'from-indigo-300 to-indigo-300' : 'from-indigo-400 hover:from-indigo-500 hover:to-indigo-500 to-indigo-500 shadow-md shadow-indigo-300 hover:scale-105'} bg-gradient-to-br  transition-all text-indigo-50 font-semibold  rounded-lg py-3 w-2/3`}
             type={"submit"}
         >
             Iniciar Sesión
