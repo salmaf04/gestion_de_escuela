ccb230083d69adcf809445370397d9b6eb3fcf54
Jose Morales Lazo <josemoraleslazo21@gmail.com>
2024-12-08 15:59:37 -0500

    delete files

--- backend/mean/mean-update/filters.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-from typing import Optional
-
-class ChangeRequest(BaseModel) :
-    state : Optional[str] = None
-    location : Optional[str] = None
--- backend/mean/mean_common/schemas.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-import uuid
-
-class MeanModel (BaseModel):
-    id: uuid.UUID
-    name: str
-    state: str
-    location: str
