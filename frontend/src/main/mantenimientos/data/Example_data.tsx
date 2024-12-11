684be4067bcdefe43977230a4eb8b50ffef0e06b
gabrielherreca <2gaherre12@gmail.com>
2024-12-10 23:15:51 -0500

    Creating Mantenimientos example data

---
+++ frontend/src/main/mantenimientos/data/Example_data.tsx
@@ -0,0 +1,28 @@
+export const header = ['Medio', 'Fecha', 'Costo'];
+
+export const data = [
+    {
+        "Id": "1",
+        "Medio": "Proyector",
+        "Fecha": "2023-01-01",
+        "Costo": '100'
+    },
+    {
+        "Id": "2",
+        "Medio": "Mapa",
+        "Fecha": "2023-02-01",
+        "Costo": '200'
+    },
+    {
+        "Id": "3",
+        "Medio": "Pizarra Interactiva",
+        "Fecha": "2023-03-01",
+        "Costo": '300'
+    },
+    {
+        "Id": "5",
+        "Medio": "Televisor de 50 pulgadas",
+        "Fecha": "2023-05-01",
+        "Costo": '500'
+    }
+];
