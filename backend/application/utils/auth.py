e02776868e9cb29413b118b0d4f6620d6477c0da
Jose Morales Lazo <josemoraleslazo21@gmail.com>
2024-12-11 22:40:21 -0500

    add password automatic generation

--- backend/application/utils/auth.py
+++ backend/application/utils/auth.py
@@ -1,4 +1,6 @@
 from passlib.context import CryptContext
+from pydantic import BaseModel
+from datetime import datetime
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -9,3 +11,13 @@ def verify_password(plain_password, hashed_password):
 # Hash password
 def get_password_hash(password):
     return pwd_context.hash(password)
+
+def get_password(schema: BaseModel) -> str:
+    name = schema.name
+    username = schema.username
+    lastname = schema.fullname 
+    today_date = datetime.now()
+    today = today_date.strftime("%d-%m-%Y")
+
+    new_password = name[0].tolower() + username[0].tolower() + lastname[0].tolower() + today
+    return new_password
