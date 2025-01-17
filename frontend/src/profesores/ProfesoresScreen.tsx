b7d90f85e2f50142c77bcdd08b013bf5cb76207b
gabrielherreca <2gaherre12@gmail.com>
2025-01-17 10:10:54 -0500

    Fixing Profesor

--- frontend/src/profesores/ProfesoresScreen.tsx
+++ frontend/src/profesores/ProfesoresScreen.tsx
@@ -17,6 +17,7 @@ export default function ProfesoresScreen() {
     useEffect(() => {
         setDataTableShow(dataTable)
     }, [dataTable]);
+    //
     useEffect(() => {
         if (searchText){
             setDataTableShow(
