3d6660b11fefbfb2affb57c52ddf12f6e0933136
gabrielherreca <2gaherre12@gmail.com>
2025-01-17 17:03:27 -0500

    Creating EstudianteGetDB model

---
+++ frontend/src/estudiantes/models/EstudianteGetDB.ts
@@ -0,0 +1,8 @@
+export type EstudianteGetDB = {
+    "name": "string",
+    "age": number,
+    "email": "string",
+    "extra_activities": boolean,
+    "username": "string",
+    "password": "string"
+}
