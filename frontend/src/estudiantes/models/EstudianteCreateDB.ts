1de9c0e8389db80bea858131adc3de9748f7a5a3
gabrielherreca <2gaherre12@gmail.com>
2025-01-18 17:48:58 -0500

    Create EstudianteCreateDB type

---
+++ frontend/src/estudiantes/models/EstudianteCreateDB.ts
@@ -0,0 +1,9 @@
+export type EstudianteCreateDB =
+    {
+        name: string,
+        age: 0,
+        email: string,
+        extra_activities: true,
+        username: string,
+        password: string
+    }
