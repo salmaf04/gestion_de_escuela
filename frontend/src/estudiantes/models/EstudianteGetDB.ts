46a5c6b435bcaf0427689fd6fb496c63c21cf5d7
gabrielherreca <2gaherre12@gmail.com>
2025-01-18 17:50:46 -0500

    Create EstudianteGetDB type

--- frontend/src/estudiantes/models/EstudianteGetDB.ts
+++ frontend/src/estudiantes/models/EstudianteGetDB.ts
@@ -1,1 +1,13 @@
-export type EstudianteGetDB
+export type EstudianteGetDB =
+    {   id : string,
+        name: string,
+        age: 0,
+        email: string,
+        extra_activities: true,
+        username: string,
+        password: string
+    }
+
+export type EstudianteGetResponse = {
+    [index: string]: EstudianteGetDB
+}
