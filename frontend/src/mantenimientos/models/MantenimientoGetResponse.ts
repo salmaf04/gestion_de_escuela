a7dee2ef41c3e77a992df7f023a7edde09e62c5b
gabrielherreca <2gaherre12@gmail.com>
2025-01-20 19:48:47 -0500

    Create Mantenimiento DB models.ts

--- frontend/src/mantenimientos/models/MantenimientoGetResponse.ts
+++
@@ -1,8 +0,0 @@
-export type MantenimientoDB = {
-        name: string
-        hourly_load: number
-        study_program: number
-        classroom_id: string
-    }
-
-export type MantenimientoGetResponse = MantenimientoDB[]
