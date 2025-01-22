fed38cea1fcc1a5e070173adf1634458fb092f98
gabrielherreca <2gaherre12@gmail.com>
2025-01-21 23:53:51 -0500

    Adding Asignatura Model

--- frontend/src/pages/asignaturas/AsignaturasScreen.tsx
+++ frontend/src/pages/asignaturas/AsignaturasScreen.tsx
@@ -1,100 +1,86 @@
-import {createContext, useEffect, useState} from "react";
+// frontend/src/pages/asignaturas/AsignaturasScreen.tsx
+import { createContext, useEffect, useState } from "react";
+import { AsignaturaGetAdapter } from "./adapters/AsignaturaGetAdapter.ts";
 import ToolBar from "./components/ToolBar.tsx";
 import Body from "./components/Body.tsx";
-import {AsignaturaCreateAdapter} from "./adapters/AsignaturaCreateAdapter.ts";
+import { AsignaturaCreateAdapter } from "./adapters/AsignaturaCreateAdapter.ts";
 import AddAsignaturaForm from "./components/AddAsignaturaForm.tsx";
-import {AsignaturaGetAdapter} from "./adapters/AsignaturaGetAdapter.ts";
-import {useEditAsignatura} from "./hooks/useEditAsignatura.ts";
-import {useCreateAsignatura} from "./hooks/useCreateAsignatura.ts";
-import {useGetAsignaturas} from "./hooks/useGetAsignatura.ts";
-import {useDeleteAsignatura} from "./hooks/useDeleteAsignatura.ts";
+import { useApiAsignatura } from "./hooks/useApiAsignatura.ts";
 
 interface IAsignaturaContext {
     searchText?: string;
     dataTable?: AsignaturaGetAdapter[];
     editting?: AsignaturaCreateAdapter;
     showModal?: boolean;
+    isGetLoading?: boolean;
+    isCreatting?: boolean;
+    isEditting?: boolean;
     setShowModal?: (text: boolean) => void;
     setEditting?: (asignatura?: AsignaturaCreateAdapter) => void;
-    isGetLoading?: boolean;
     setSearchText?: (text: string) => void;
     onDeleteTableItem?: (index: string) => void;
     onEditTableItem?: (asignaturaEdit: AsignaturaCreateAdapter) => void;
     onAddTableItem?: (asignaturaEdit: AsignaturaCreateAdapter) => void;
-    isEditting?: boolean;
-    isCreatting?: boolean;
 }
 
-export const AsignaturaContext = createContext<IAsignaturaContext>(
-    {}
-);
+export const AsignaturaContext = createContext<IAsignaturaContext>({});
 
 export default function AsignaturasScreen() {
     const [searchText, setSearchText] = useState('');
-    const [editting, setEditting] = useState<AsignaturaCreateAdapter | undefined>()
-    const [showModal, setShowModal] = useState(false)
+    const [editting, setEditting] = useState<AsignaturaCreateAdapter | undefined>();
+    const [showModal, setShowModal] = useState(false);
+    const [isCreating, setIsCreating] = useState(false);
+    const [isEditing, setIsEditing] = useState(false);
     const {
-        editedAsignatura,
-        isLoading: isEditting,
-        editAsignatura
-    } = useEditAsignatura()
-    const {
-        newAsignatura,
-        isLoading: isCreatting,
-        createAsignatura
-    } = useCreateAsignatura()
-
-    const {
-        isGetLoading,
         asignaturas,
+        deleteAsignatura,
+        createAsignatura,
+        updateAsignatura,
         getAsignaturas,
-    } = useGetAsignaturas()
-
-    const {
-       deleteAsignatura,
-        deletedAsignaturaId,
-    } = useDeleteAsignatura()
+    } = useApiAsignatura();
 
     useEffect(() => {
-        getAsignaturas()
-    }, [editedAsignatura, newAsignatura , deletedAsignaturaId]);
+        getAsignaturas();
+    }, []);
 
-    const onDeleteTableItem = (deletedAsignaturaId : string ) => {
-        deleteAsignatura(deletedAsignaturaId)
-    }
+    const onDeleteTableItem = (deletedAsignaturaId: string) => {
+        deleteAsignatura(deletedAsignaturaId);
+    };
 
     const onEditTableItem = (asignaturaEdit: AsignaturaCreateAdapter) => {
-        editAsignatura(asignaturaEdit)
-
-    }
+        setIsEditing(true);
+        updateAsignatura(asignaturaEdit);
+        setIsEditing(false);
+    };
 
     const onAddTableItem = (asignatura: AsignaturaCreateAdapter) => {
-        createAsignatura(asignatura)
-    }
+        setIsCreating(true);
+        createAsignatura(asignatura);
+        setIsCreating(false);
+    };
+
     return (
         <AsignaturaContext.Provider value={{
-            isGetLoading: isGetLoading,
             dataTable: asignaturas,
             searchText: searchText,
             editting: editting,
             showModal: showModal,
+            isCreatting: isCreating,
+            isEditting: isEditing,
             setShowModal: setShowModal,
             setEditting: setEditting,
             setSearchText: setSearchText,
             onDeleteTableItem: onDeleteTableItem,
             onEditTableItem: onEditTableItem,
             onAddTableItem: onAddTableItem,
-            isEditting: isEditting,
-            isCreatting: isCreatting
-        }
-        }>
+        }}>
             <div className={'w-full h-dvh flex flex-col'}>
-                <ToolBar/>
+                <ToolBar />
                 <Body />
                 {(showModal || editting) &&
                     <AddAsignaturaForm />
                 }
             </div>
         </AsignaturaContext.Provider>
-    )
+    );
 }
