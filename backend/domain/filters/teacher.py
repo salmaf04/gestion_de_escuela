cfceca8839a66999a8b38c2b22e722a88e0183e9
joselazo21 <josemoraleslazo21@gmail.com>
2025-01-22 00:29:50 -0500

    Add if to teacher filter

--- backend/domain/filters/teacher.py
+++ backend/domain/filters/teacher.py
@@ -2,8 +2,10 @@ from sqlalchemy_filterset import FilterSet, Filter, RangeFilter, BooleanFilter
 from pydantic import BaseModel, Field
 from backend.domain.models.tables import TeacherTable
 from typing import Optional
+import uuid
 
 class TeacherFilterSet(FilterSet):
+    id = Filter(TeacherTable.id)
     name = Filter(TeacherTable.name)
     email = Filter(TeacherTable.email)
     specialty = Filter(TeacherTable.specialty)
@@ -11,6 +13,7 @@ class TeacherFilterSet(FilterSet):
     experience = RangeFilter(TeacherTable.experience)
 
 class TeacherFilterSchema(BaseModel):
+    id : uuid.UUID
     name : str | None = None
     email : str | None = None
     specialty : str | None = None
