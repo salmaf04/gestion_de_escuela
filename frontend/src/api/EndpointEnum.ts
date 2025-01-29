e66f0226e943a25e3e813db8879ea102979cda7a
gabrielherreca <2gaherre12@gmail.com>
2025-01-29 10:04:52 -0500

    Fix endpoint route of note

--- frontend/src/api/EndpointEnum.ts
+++ frontend/src/api/EndpointEnum.ts
@@ -11,6 +11,6 @@ export enum EndpointEnum {
     FILTRO_DE_MANTENIMENTO = 'mean_maintenance/?mainteniance_by_classroom_filter=true',
     COSTO_PROMEDIO = 'mean_maintenance/?date_filter=true',
     VALORACION_PROMEDIO_PROFESOR = 'teacher/?better_than_eight=true ',
-    VALORACION_PROMEDIO_ESTUDIANTE = 'student/?student_note_less_than_fifty=true',
+    VALORACION_PROMEDIO_ESTUDIANTE = 'note/?less_than_fifty=true',
     SALARIOS_PROFESORES = 'teacher/?sanctions=true'
 }
