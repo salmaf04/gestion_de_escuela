4b13d850869ded11d7b52b425d5df40bf6c10421
gabrielherreca <2gaherre12@gmail.com>
2025-01-29 08:44:01 -0500

    Fix endpoint route

--- frontend/src/api/EndpointEnum.ts
+++ frontend/src/api/EndpointEnum.ts
@@ -10,7 +10,7 @@ export enum EndpointEnum {
     ESP_PROFESOR = 'teacher/?technology_classroom=true',
     FILTRO_DE_MANTENIMENTO = 'mean_maintenance/?mainteniance_by_classroom_filter=true',
     COSTO_PROMEDIO = 'mean_maintenance/?date_filter=true',
-    VALORACION_PROMEDIO_PROFESOR = 'teacher/better_than_eight ',
-    VALORACION_PROMEDIO_ESTUDIANTE = 'student/?less_than_fifty=true',
+    VALORACION_PROMEDIO_PROFESOR = 'teacher/?better_than_eight=true ',
+    VALORACION_PROMEDIO_ESTUDIANTE = 'student/?student_note_less_than_fifty=true',
     SALARIOS_PROFESORES = 'teacher/?sanctions=true'
 }
