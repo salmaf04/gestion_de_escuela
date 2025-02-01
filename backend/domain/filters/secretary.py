711efec438c732f1209081ae69db0bd60e1c7a15
Jose Morales Lazo <josemoraleslazo21@gmail.com>
2025-02-01 18:49:10 -0500

    Add secretary get

--- backend/domain/filters/secretary.py
+++ backend/domain/filters/secretary.py
@@ -1,8 +1,24 @@
 from pydantic import BaseModel
 from typing import Optional
+from sqlalchemy_filterset import FilterSet, Filter, RangeFilter, BooleanFilter
+from pydantic import BaseModel
+from backend.domain.models.tables import SecretaryTable
 
+class SecretaryFilterSet(FilterSet):
+    id = Filter(SecretaryTable.id)
+    name = Filter(SecretaryTable.name)
+    email = Filter(SecretaryTable.email)
+    username = Filter(SecretaryTable.username)
+
+class SecretaryFilterSchema(BaseModel):
+    id : str | None = None
+    name : str | None = None
+    email : str | None = None
+    username : str | None = None
+    
 class SecretaryChangeRequest(BaseModel) :
     name : Optional[str] = None
     username : Optional[str] = None
     email : Optional[str] = None
     hash_password : Optional[str] = None
+
