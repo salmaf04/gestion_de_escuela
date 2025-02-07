b819c856c2ca7b007868543728519260306f8b0c
AdrianSouto <adrianalejandrosm03@gmail.com>
2025-02-07 12:36:53 -0500

    fix teacher access to add subject

--- frontend/src/pages/asignaturas/components/ToolBar.tsx
+++ frontend/src/pages/asignaturas/components/ToolBar.tsx
@@ -4,6 +4,8 @@ import {useContext} from "react";
 import {AsignaturaContext} from "../AsignaturasScreen.tsx";
 import ExportButton from "../../../components/ExportButton.tsx";
 import {AsignaturaGetAdapter} from "../adapters/AsignaturaGetAdapter.ts";
+import {AppContext} from "../../../App.tsx";
+import {RolesEnum} from "../../../api/RolesEnum.ts";
 
 export default function ToolBar() {
     const {
@@ -12,9 +14,7 @@ export default function ToolBar() {
         setShowModal,
         dataTable
     } = useContext(AsignaturaContext)
-
-
-
+    const {allowRoles} = useContext(AppContext)
 
     return (
             <div className={'self-end w-2/3 my-4 h-1/6 flex items-center justify-between px-5'}>
@@ -24,7 +24,9 @@ export default function ToolBar() {
                 }}/>
 
                 <ExportButton title={'Asignaturas'} headers = {AsignaturaGetAdapter.Properties.slice(1)} data={dataTable}></ExportButton>
-                <AddButton onClick={() => setShowModal!(true)}/>
+                {allowRoles!([RolesEnum.SECRETARY]) &&
+                    <AddButton onClick={() => setShowModal!(true)}/>
+                }
             </div>
     )
 }
