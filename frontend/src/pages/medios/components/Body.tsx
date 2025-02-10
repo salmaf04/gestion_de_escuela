2969761c09bc96e6a48d953ebb654c39963a7d0d
AdrianSouto <adrianalejandrosm03@gmail.com>
2025-02-09 23:13:51 -0500

    new alerta medio reemplazado

--- frontend/src/pages/medios/components/Body.tsx
+++ frontend/src/pages/medios/components/Body.tsx
@@ -10,50 +10,63 @@ import {RolesEnum} from "../../../api/RolesEnum.ts";
 import {DBObject} from "../../../types.ts";
 import DevolverIcon from "../../../assets/devolver.svg";
 
-export default function Body(){
+export default function Body() {
     const {dataTable, setEditting, onDeleteTableItem, isLoading} = useContext(MedioContext)
     const {solicitarMedio, devolverMedio} = useApiMedio()
-    const {medios, allowRoles, personalId} = useContext(AppContext)
+    const {medios, allowRoles, personalId, setError} = useContext(AppContext)
+    let actions = []
+    if (allowRoles!([RolesEnum.TEACHER])) {
+        actions.push({
+            action: (row: DBObject) => {
+                solicitarMedio!({mean_id: row.id})
+            },
+            lineColor: 'bg-green-500',
+            hoverColor: 'hover:bg-green-100',
+            title: "Solicitar",
+            icon: <img src={SolicitarIcon} alt={'Solicitar'}/>,
+            isVisible: (id) => !medios!.find((aula) => aula.id === id)!.requested_by
+        }, {
+            action: (row) => {
+                devolverMedio(row.id)
+            },
+            lineColor: 'bg-red-500',
+            hoverColor: 'hover:bg-red-100',
+            title: "Devolver",
+            icon: <img src={DevolverIcon} alt={'Devolver'}/>,
+            isVisible: (id) => medios!.find((aula) => aula.id === id)!.requested_by === personalId
+        })
+    }
+    if (allowRoles!([RolesEnum.ADMIN])){
+        actions.push(
+            {
+                action: () => {
+                    setError!(new Error("Este medio necesita ser reemplazado"))
+                },
+                lineColor: 'bg-amber-500',
+                hoverColor: 'hover:bg-amber-100',
+                title: "Alerta",
+                icon: <img src={AlertIcon} alt={'Alerta'}/>,
+                isVisible: (row: string) => medios!.find!((medio) => medio.id === row)!.to_be_replaced
+            }
+        )
+    }
 
-    return(
-            <Table
-                className={'h-5/6'}
-                isLoading={isLoading!}
-                Data={dataTable ?? []} header={MedioGetAdapter.Properties.slice(1)}
-                   onRemoveRow={(index) => {
-                       onDeleteTableItem!(index)
-                   }}
-                   onEditRow={(index) => {
-                       if (allowRoles!([RolesEnum.ADMIN])) {
-                           const item = dataTable!.find((item) => item.id === index)
-                           setEditting!(item!)
-                       }
-                   }}
-                actions={
-                    allowRoles!([RolesEnum.TEACHER]) ? [
-                        {
-                            action: (row: DBObject) => {
-                                solicitarMedio!({mean_id: row.id})
-                            },
-                            lineColor: 'bg-green-500',
-                            hoverColor: 'hover:bg-green-100',
-                            title: "Solicitar",
-                            icon: <img src={SolicitarIcon} alt={'Solicitar'}/>,
-                            isVisible: (id)=> !medios!.find((aula)=>aula.id === id)!.requested_by
-                        },
-                        {
-                            action: (row) => {
-                                devolverMedio(row.id)
-                            },
-                            lineColor: 'bg-red-500',
-                            hoverColor: 'hover:bg-red-100',
-                            title: "Devolver",
-                            icon: <img src={DevolverIcon} alt={'Devolver'}/>,
-                            isVisible: (id)=> medios!.find((aula)=>aula.id === id)!.requested_by === personalId
-                        }
-                    ]: []
+    return (
+        <Table
+            className={'h-5/6'}
+            isLoading={isLoading!}
+            Data={dataTable ?? []} header={MedioGetAdapter.Properties.slice(1)}
+            onRemoveRow={(index) => {
+                onDeleteTableItem!(index)
+            }}
+            onEditRow={(index) => {
+                if (allowRoles!([RolesEnum.ADMIN])) {
+                    const item = dataTable!.find((item) => item.id === index)
+                    setEditting!(item!)
                 }
-                isRemoveActive={allowRoles!([RolesEnum.ADMIN])}
-            />
+            }}
+            actions={actions}
+            isRemoveActive={allowRoles!([RolesEnum.ADMIN])}
+        />
     )
 }
